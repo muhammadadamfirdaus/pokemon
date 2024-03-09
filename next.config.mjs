75911440d7216138af64e6c6c020e86501b1a686
muhammadadamfirdaus <adam@muhammadadamfirdaus.com>
2024-03-10 01:26:37 +0700

    revert next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: {
-    DEVELOPMENT: process.env.NEXT_PUBLIC_DEVELOPMENT,
-    VERCEL_URL: process.env.NEXT_PUBLIC_VERCEL_URL,
-  },
   images: {
     remotePatterns: [
       {
